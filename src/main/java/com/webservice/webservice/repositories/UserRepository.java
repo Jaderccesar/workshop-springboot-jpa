4f500e6f3fde986f188a315a8e730f231d2b9156
jaderccesar <jadercesarvanderlinde12@gmail.com>
2024-09-15 18:45:21 -0300

    Service layer, component registration

--- src/main/java/com/webservice/webservice/repositories/UserRepository.java
+++ src/main/java/com/webservice/webservice/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.webservice.webservice.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 }
