c3994d94f2c621de8511de5eb437a70f44db574a
jaderccesar <jadercesarvanderlinde12@gmail.com>
2024-09-21 15:25:49 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/webservice/webservice/repositories/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.webservice.webservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.webservice.webservice.entities.OrderItem;
+import com.webservice.webservice.entities.pk.OrderItemPk;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPk>{
+	
+}
